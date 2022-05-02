5637056e7d07f79bfe988b46281f1c0a0f6a19f9
Jon Taylor <jonnytest1101@icloud.com>
2022-05-02 16:33:12 -0500

    Added two tests

--- __tests__/GravatarTest.php
+++ __tests__/GravatarTest.php
@@ -11,5 +11,34 @@ use SweatyShell\Gravatar;
 
 class GravatarTest extends \PHPUnit\Framework\TestCase
 {
-    public function
+    protected string $testEmail = 'jonnytest1101@icloud.com';
+    protected string $gravatarUrl = 'https://www.gravatar.com/avatar/';
+
+    protected function getHashedEmailed(): string
+    {
+        return md5($this->testEmail);
+    }
+
+    protected function getExpectedUrl(): string
+    {
+        return sprintf("%s%s", $this->gravatarUrl, $this->getHashedEmailed());
+    }
+
+    public function testEmailIsHashed():void
+    {
+        $gravatar = new Gravatar;
+        $gravatar->setEmail($this->testEmail);
+        $this->assertEquals($this->getHashedEmailed(), $gravatar->getHash());
+    }
+
+    public function testToString(): void
+    {
+        $gravatar = new Gravatar;
+        $gravatar->setEmail($this->testEmail);
+
+        $expected = $this->getExpectedUrl();
+        $actual = "$gravatar";
+
+        $this->assertEquals($expected, $actual);
+    }
 }
